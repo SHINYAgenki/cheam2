7b9bbb14a772223cb09c63d8a3f498433beaa0da
Shinya Genki <shinyagenki.uec22@gmail.com>
2024-11-12 23:06:40 +0900

    aaa

---
+++ SecretKey.c
@@ -0,0 +1,443 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// S-box look-up table
+const unsigned char g_sbox[256] = {
+    0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5,
+    0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
+    0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0,
+    0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
+    0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc,
+    0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
+    0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a,
+    0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75,
+    0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0,
+    0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84,
+    0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b,
+    0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf,
+    0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85,
+    0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8,
+    0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5,
+    0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2,
+    0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17,
+    0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73,
+    0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88,
+    0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb,
+    0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c,
+    0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79,
+    0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9,
+    0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08,
+    0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6,
+    0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,
+    0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e,
+    0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
+    0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94,
+    0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
+    0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68,
+    0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16,
+};
+
+const unsigned char g_inv_sbox[256] = {
+    0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38,
+    0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB,
+    0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87,
+    0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB,
+    0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D,
+    0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E,
+    0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2,
+    0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25,
+    0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16,
+    0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92,
+    0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA,
+    0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84,
+    0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A,
+    0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06,
+    0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02,
+    0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B,
+    0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA,
+    0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73,
+    0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85,
+    0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E,
+    0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89,
+    0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B,
+    0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20,
+    0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4,
+    0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31,
+    0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F,
+    0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D,
+    0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF,
+    0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0,
+    0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61,
+    0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26,
+    0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D,
+};
+
+unsigned char gmul(unsigned char a, unsigned char b) {
+    // irreducible polynomial: X^8 + X^4 + X^3 + X + 1
+	unsigned char p = 0;
+	unsigned char counter;
+	unsigned char hi_bit_set;
+	for(counter = 0; counter < 8; counter++) {
+		if((b & 1) == 1)
+			p ^= a;
+		hi_bit_set = (a & 0x80);
+		a <<= 1;
+		if(hi_bit_set == 0x80)
+			a ^= 0x1b;
+		b >>= 1;
+	}
+	return p;
+}
+
+void SubBytes(unsigned char *dst, unsigned char *src) {
+    for (int i = 0; i < 16; i++) {
+        dst[i] = g_sbox[src[i]];
+    }
+}
+
+void invSubBytes(unsigned char *dst, unsigned char *src) {
+    for (int i = 0; i < 16; i++) {
+        dst[i] = g_inv_sbox[src[i]];
+    }
+}
+
+void ShiftRows(unsigned char *dst, unsigned char *src) {
+    unsigned char tmp;
+
+    // 1st row
+    dst[0] = src[0];
+    dst[4] = src[4];
+    dst[8] = src[8];
+    dst[12] = src[12];
+
+    // 2nd row
+    tmp = src[1];
+    dst[1] = src[5];
+    dst[5] = src[9];
+    dst[9] = src[13];
+    dst[13] = tmp;
+
+    // 3rd row
+    tmp = src[2];
+    dst[2] = src[10];
+    dst[10] = tmp;
+    tmp = src[6];
+    dst[6] = src[14];
+    dst[14] = tmp;
+
+    // 4th row
+    tmp = src[3];
+    dst[3] = src[15];
+    dst[15] = src[11];
+    dst[11] = src[7];
+    dst[7] = tmp;
+}
+
+void invShiftRows(unsigned char *dst, unsigned char *src) {
+    unsigned char tmp;
+
+    // 1st row
+    dst[0] = src[0];
+    dst[4] = src[4];
+    dst[8] = src[8];
+    dst[12] = src[12];
+
+    // 2nd row
+    tmp = src[13];
+    dst[13] = src[9];
+    dst[9] = src[5];
+    dst[5] = src[1];
+    dst[1] = tmp;
+
+    // 3rd row
+    tmp = src[2];
+    dst[2] = src[10];
+    dst[10] = tmp;
+    tmp = src[6];
+    dst[6] = src[14];
+    dst[14] = tmp;
+
+    // 4th row
+    tmp = src[3];
+    dst[3] = src[7];
+    dst[7] = src[11];
+    dst[11] = src[15];
+    dst[15] = tmp;
+}
+
+
+void MixColumns(unsigned char *dst, unsigned char *src) {
+    unsigned char tmp[16];
+    tmp[0] = gmul(src[0],2)^gmul(src[1],3)^gmul(src[2],1)^gmul(src[3],1);
+    tmp[1] = gmul(src[0],1)^gmul(src[1],2)^gmul(src[2],3)^gmul(src[3],1);
+    tmp[2] = gmul(src[0],1)^gmul(src[1],1)^gmul(src[2],2)^gmul(src[3],3);
+    tmp[3] = gmul(src[0],3)^gmul(src[1],1)^gmul(src[2],1)^gmul(src[3],2);
+
+    tmp[4] = gmul(src[4],2)^gmul(src[5],3)^gmul(src[6],1)^gmul(src[7],1);
+    tmp[5] = gmul(src[4],1)^gmul(src[5],2)^gmul(src[6],3)^gmul(src[7],1);
+    tmp[6] = gmul(src[4],1)^gmul(src[5],1)^gmul(src[6],2)^gmul(src[7],3);
+    tmp[7] = gmul(src[4],3)^gmul(src[5],1)^gmul(src[6],1)^gmul(src[7],2);
+
+    tmp[8]  = gmul(src[8],2)^gmul(src[9],3)^gmul(src[10],1)^gmul(src[11],1);
+    tmp[9]  = gmul(src[8],1)^gmul(src[9],2)^gmul(src[10],3)^gmul(src[11],1);
+    tmp[10] = gmul(src[8],1)^gmul(src[9],1)^gmul(src[10],2)^gmul(src[11],3);
+    tmp[11] = gmul(src[8],3)^gmul(src[9],1)^gmul(src[10],1)^gmul(src[11],2);
+
+    tmp[12] = gmul(src[12],2)^gmul(src[13],3)^gmul(src[14],1)^gmul(src[15],1);
+    tmp[13] = gmul(src[12],1)^gmul(src[13],2)^gmul(src[14],3)^gmul(src[15],1);
+    tmp[14] = gmul(src[12],1)^gmul(src[13],1)^gmul(src[14],2)^gmul(src[15],3);
+    tmp[15] = gmul(src[12],3)^gmul(src[13],1)^gmul(src[14],1)^gmul(src[15],2);
+
+    memcpy(dst, tmp, 16);
+}
+
+void invMixColumns(unsigned char *dst, unsigned char *src) {
+    unsigned char tmp[16];
+    tmp[0] = gmul(src[0],14)^gmul(src[1],11)^gmul(src[2],13)^gmul(src[3],9);
+    tmp[1] = gmul(src[0],9)^gmul(src[1],14)^gmul(src[2],11)^gmul(src[3],13);
+    tmp[2] = gmul(src[0],13)^gmul(src[1],9)^gmul(src[2],14)^gmul(src[3],11);
+    tmp[3] = gmul(src[0],11)^gmul(src[1],13)^gmul(src[2],9)^gmul(src[3],14);
+
+    tmp[4] = gmul(src[4],14)^gmul(src[5],11)^gmul(src[6],13)^gmul(src[7],9);
+    tmp[5] = gmul(src[4],9)^gmul(src[5],14)^gmul(src[6],11)^gmul(src[7],13);
+    tmp[6] = gmul(src[4],13)^gmul(src[5],9)^gmul(src[6],14)^gmul(src[7],11);
+    tmp[7] = gmul(src[4],11)^gmul(src[5],13)^gmul(src[6],9)^gmul(src[7],14);
+
+    tmp[8] = gmul(src[8],14)^gmul(src[9],11)^gmul(src[10],13)^gmul(src[11],9);
+    tmp[9] = gmul(src[8],9)^gmul(src[9],14)^gmul(src[10],11)^gmul(src[11],13);
+    tmp[10] = gmul(src[8],13)^gmul(src[9],9)^gmul(src[10],14)^gmul(src[11],11);
+    tmp[11] = gmul(src[8],11)^gmul(src[9],13)^gmul(src[10],9)^gmul(src[11],14);
+
+    tmp[12] = gmul(src[12],14)^gmul(src[13],11)^gmul(src[14],13)^gmul(src[15],9);
+    tmp[13] = gmul(src[12],9)^gmul(src[13],14)^gmul(src[14],11)^gmul(src[15],13);
+    tmp[14] = gmul(src[12],13)^gmul(src[13],9)^gmul(src[14],14)^gmul(src[15],11);
+    tmp[15] = gmul(src[12],11)^gmul(src[13],13)^gmul(src[14],9)^gmul(src[15],14);
+
+    memcpy(dst, tmp, 16);
+}
+
+
+void AddRoundKey(unsigned char *dst, unsigned char *src, unsigned char *key) {
+    for (int i = 0; i < 16; i++) {
+        dst[i] = src[i] ^ key[i];
+    }
+}
+
+void KeyExpansion(unsigned char *w, unsigned char *init_key) {
+    unsigned char Rcon[16] = {
+        0x01,0x01,0x02,0x04,0x08,0x10,0x20,0x40,
+        0x80,0x1b,0x36,0x6c,0xd8,0xab,0x4d,0x9a,
+    };
+	unsigned char temp,temp1,temp2,temp3,temp4;
+	const int Nb = 4;
+	const int Nk = 4;
+	const int Nr = 10;
+
+    memcpy(w, init_key, 16);
+
+	int i = 4*Nk;
+    while(i < 4*Nb*(Nr+1)) {
+        temp1 = w[i-4];
+        temp2 = w[i-3];
+        temp3 = w[i-2];
+        temp4 = w[i-1];
+	    if (i % (4*Nk) == 0){
+            // RotWord and SubWord
+            temp = temp1;
+            temp1 = g_sbox[temp2];
+            temp2 = g_sbox[temp3];
+            temp3 = g_sbox[temp4];
+            temp4 = g_sbox[temp];
+            // Rcon
+            temp1 = temp1 ^ Rcon[i/(4*Nk)];
+            temp2 = temp2 ^ 0;
+            temp3 = temp3 ^ 0;
+            temp4 = temp4 ^ 0;
+	    }
+	    else if (Nk > 6 && (i%Nk) == 4) {
+            //SubWord
+            temp1 = g_sbox[temp1];
+            temp2 = g_sbox[temp2];
+            temp3 = g_sbox[temp3];
+            temp4 = g_sbox[temp4];
+		}
+        w[i]   = w[i-4*Nk]   ^ temp1;
+        w[i+1] = w[i-4*Nk+1] ^ temp2;
+        w[i+2] = w[i-4*Nk+2] ^ temp3;
+        w[i+3] = w[i-4*Nk+3] ^ temp4;
+        i += 4;
+    }
+}
+
+void Encrypt(unsigned char *dst, unsigned char *src, unsigned char *init_key) {
+    unsigned char key[16]; //それぞれのラウンド鍵
+    unsigned char w[176]; //ラウンド鍵をまとめたもの16×１１
+    unsigned char state[16]; //16bite
+    int i;
+
+    KeyExpansion(w, init_key); //全てのラウンド鍵の生成、初期鍵からラウンド鍵を生成し配列wに格納
+
+    //暗号(Cipher)
+    //0ラウンド
+    AddRoundKey(state, src, init_key); 
+
+    //1ラウンドのSubBytesまで
+    SubBytes(dst, state);
+    //ShiftRows(state, state);
+    //MixColumns(dst, state);
+    //AddRoundKey(dst, state, key);
+}
+
+//差分し、差分された中間値について比を確認する関数
+int sabun0(unsigned char *pt1, unsigned char *pt2, int a1, int a2){
+    unsigned char pt[16] = {0};
+
+    //PT1，PT2の差分をPTに格納
+    for (int i=0; i<=15; i++){
+        pt[i] = pt1[i]^pt2[i];
+    }
+    //PTのpt[a1]とpt[a2]について、比を確認する
+    if( gmul( pt[a1], 0x09 ) == gmul( pt[a2], 0x0e ) || 
+        gmul( pt[a1], 0x0e ) == gmul( pt[a2], 0x0b ) || 
+        gmul( pt[a1], 0x0b ) == gmul( pt[a2], 0x0d ) || 
+        gmul( pt[a1], 0x0d ) == gmul( pt[a2], 0x09 ) ){
+        return 1;
+    }
+    return 0;
+}
+
+
+int main() {
+    //1列目
+    //平文ペア1つ目 0 5 10 15
+    unsigned char P1[16] = {0x3D, 0xCF, 0xD6, 0x74, 0xBD, 0xB2, 0xEF, 0x05, 0xC4, 0x66, 0x91, 0x68, 0x6F, 0x24, 0xA8, 0x9F};//平文P1
+    unsigned char P2[16] = {0xAF, 0xCF, 0xD6, 0x74, 0xBD, 0x79, 0xEF, 0x05, 0xC4, 0x66, 0x59, 0x68, 0x6F, 0x24, 0xA8, 0x75}; //平文P2
+    // //平文ペア2つ目
+    unsigned char P3[16] = {0x52, 0x71, 0x3F, 0xCF, 0x46, 0x07, 0x9F, 0xF8, 0x67, 0x24, 0x82, 0x60, 0x50, 0x66, 0x4A, 0x9C};
+    unsigned char P4[16] = {0xD6, 0x71, 0x3F, 0xCF, 0x46, 0x66, 0x9F, 0xF8, 0x67, 0x24, 0xE6, 0x60, 0x50, 0x66, 0x4A, 0xC5};
+
+    //2列目
+    //平文ペア1つ目 3 4 9 14
+    unsigned char P5[16] = {0x3F, 0x2A, 0x26, 0x27, 0xFA, 0x16, 0xDB, 0x7A, 0xF4, 0xE6, 0x0F, 0x25, 0x70, 0xDA, 0x28, 0x7A};
+    unsigned char P6[16] = {0x3F, 0x2A, 0x26, 0x82, 0x10, 0x16, 0xDB, 0x7A, 0xF4, 0x0D, 0x0F, 0x25, 0x70, 0xDA, 0xA8, 0x7A};
+
+    // //平文ペア2つ目
+    unsigned char P7[16] = {0x99, 0x88, 0x14, 0xD4, 0xD6, 0x37, 0xC2, 0xA8, 0xC7, 0x68, 0x24, 0x53, 0xE2, 0xB7, 0x57, 0xA7};
+    unsigned char P8[16] = {0x99, 0x88, 0x14, 0x61, 0xF7, 0x37, 0xC2, 0xA8, 0xC7, 0xF7, 0x24, 0x53, 0xE2, 0xB7, 0x2A, 0xA7};
+
+    // //3列目
+    // //平文ペア1つ目 2 7 8 13
+    unsigned char P9[16] = {0xB9, 0x45, 0x15, 0x61, 0xB4, 0x78, 0xF1, 0x49, 0xAE, 0x2B, 0x4B, 0xE1, 0xD1, 0x5A, 0xC2, 0xDB};
+    unsigned char P10[16] = {0xB9, 0x45, 0xBD, 0x61, 0xB4, 0x78, 0xF1, 0xC0, 0xF7, 0x2B, 0x4B, 0xE1, 0xD1, 0x4E, 0xC2, 0xDB};
+
+    // //平文ペア2つ目
+    unsigned char P11[16] = {0xDF, 0xDD, 0xCE, 0xF4, 0x09, 0xF8, 0x3B, 0xE3, 0x59, 0x2E, 0x46, 0xBE, 0x08, 0x1F, 0x57, 0x25};
+    unsigned char P12[16] = {0xDF, 0xDD, 0xA0, 0xF4, 0x09, 0xF8, 0x3B, 0x8E, 0x91, 0x2E, 0x46, 0xBE, 0x08, 0x4A, 0x57, 0x25};
+
+    // //平文ペア3つ目
+    unsigned char P11_a[16] = {0x3E, 0x24, 0x56, 0xF2, 0xEA, 0xF1, 0xC6, 0x72, 0x45, 0x32, 0xD7, 0x75, 0x51, 0xAB, 0x7A, 0x8E};
+    unsigned char P12_b[16] = {0x3E, 0x24, 0xDC, 0xF2, 0xEA, 0xF1, 0xC6, 0x77, 0xAF, 0x32, 0xD7, 0x75, 0x51, 0xC9, 0x7A, 0x8E};
+
+    // //4列目
+    // //平文ペア1つ目 1 6 11 12
+    unsigned char P13[16] = {0x19, 0x66, 0x50, 0x04, 0xF7, 0x6D, 0xA3, 0xAE, 0xB6, 0x24, 0xB5, 0xB8, 0x3C, 0x7E, 0xC7, 0x1F};
+    unsigned char P14[16] = {0x19, 0x57, 0x50, 0x04, 0xF7, 0x6D, 0x4B, 0xAE, 0xB6, 0x24, 0xB5, 0x48, 0x3E, 0x7E, 0xC7, 0x1F};
+
+    // //平文ペア2つ目
+    unsigned char P15[16] = {0x55, 0x20, 0x32, 0x58, 0x65, 0x78, 0xEB, 0x5B, 0x63, 0xCF, 0xDD, 0x5B, 0xE6, 0x9C, 0x21, 0xF5};
+    unsigned char P16[16] = {0x55, 0x33, 0x32, 0x58, 0x65, 0x78, 0x7C, 0x5B, 0x63, 0xCF, 0xDD, 0x64, 0xC1, 0x9C, 0x21, 0xF5};
+
+    unsigned char P15_a[16] = {0x3A, 0xA0, 0xF7, 0x4A, 0x6E, 0xCF, 0x38, 0x0D, 0xF9, 0x49, 0xCA, 0x10, 0x70, 0x6A, 0x7F, 0x6E};
+    unsigned char P15_b[16] = {0x3A, 0x89, 0xF7, 0x4A, 0x6E, 0xCF, 0x54, 0x0D, 0xF9, 0x49, 0xCA, 0x23, 0x73, 0x6A, 0x7F, 0x6E};
+
+    //1ラウンド目SubBytes処理終了後の中間値
+    unsigned char C1[16] = {};
+    unsigned char C2[16] = {};
+    unsigned char C3[16] = {};
+    unsigned char C4[16] = {};
+
+    unsigned char Key[16] = {0}; //候補鍵
+    unsigned char last_key[16] = {0}; //条件に当てはまる鍵
+
+    int state1, state2, state3, state4;
+    int i;  
+
+    //1~4列目に差分があるときSubytes処理後に影響がある位置をそれぞれ示す
+    int type[4][4] = {{0, 5, 10, 15}, {3, 4, 9, 14}, {8, 13, 2, 7}, {12, 1, 6, 11}};
+    //探索に使用する平文ペアを2ペアずつにまとめた 
+    unsigned char *pList[4][4] = { {P1, P2, P3, P4},
+                                   {P5, P6, P7, P8},
+                                   {P9, P10, P11_a, P12_b},
+                                   {P13, P14, P15_b, P15_a}};
+
+    //行columnごとに探索を行う
+    for (int column = 0; column <4; column++){
+        int a = type[column][0];
+        int b = type[column][1];
+        int c = type[column][2];
+        int d = type[column][3];
+
+        unsigned char *X = pList[column][0];
+        unsigned char *Y = pList[column][1];
+        unsigned char *Z = pList[column][2];
+        unsigned char *W = pList[column][3];
+
+        //以下候補鍵を探す
+        //鍵のKey[a],Key[b]にまず0から255までfor文をまわす
+        for ( state1=0; state1<=255; state1++ ){
+            for( state2=0; state2<=255; state2++ ){
+                Key[a] = (unsigned char)state1;
+                Key[b] = (unsigned char)state2;
+                //Key[a],Key[b]が入力されたKeyについて、Encryptする
+                Encrypt(C1, X, Key);
+                Encrypt(C2, Y, Key);
+                Encrypt(C3, Z, Key);
+                Encrypt(C4, W, Key);
+
+                //Subbytes後のC1,C2の差分,C3,C4の差分について、1行目と1行目の比がKeyとして適しているか判定する
+                //適していない場合、次のKeyを試す
+                if ( sabun0( C1, C2 , a, b ) == 1  && sabun0 ( C3, C4, a, b ) == 1 ){
+                    //比が適していいた場合、Key[c]も0から255までfor文をまわす
+                    for( state3=0; state3<=255; state3++ ){
+                        Key[c] = (unsigned char)state3;
+                        //Key[c]を追加しているため、再度Encryptする
+                        Encrypt(C1, X, Key);
+                        Encrypt(C2, Y, Key);
+                        Encrypt(C3, Z, Key);
+                        Encrypt(C4, W, Key);
+
+                        //同様に、Subbytes後のC1,C2の差分,C3,C4の差分について、2行目と3行目の比がKeyとして適しているか判定する
+                        if ( sabun0 ( C1, C2, b, c ) == 1 && sabun0 ( C3, C4, b, c ) == 1 ){
+                            //比が適していいた場合、Key[d]も0から255までfor文をわます
+                            for( state4=0; state4<=255; state4++ ){
+                                Key[d] = (unsigned char)state4;
+                                //Key[a]~[d]が入力されたKeyで、再度Encryptする
+                                Encrypt(C1, X, Key);
+                                Encrypt(C2, Y, Key);
+                                Encrypt(C3, Z, Key);
+                                Encrypt(C4, W, Key);
+
+                                //同様に、Subbytes後のC1,C2の差分,C3,C4の差分について、3行目と4行目の比がKeyとして適しているか判定する
+                                if ( sabun0 ( C1, C2, c, d ) == 1  && sabun0 ( C3, C4, c, d ) == 1 ){
+                                    //比が適していた場合、候補鍵Keyではなく、鍵last_keyとして入力
+                                    last_key[a] = (unsigned char)state1;
+                                    last_key[b] = (unsigned char)state2;
+                                    last_key[c] = (unsigned char)state3;
+                                    last_key[d] = (unsigned char)state4;
+
+                                    //条件に合った鍵last_keyの出力
+                                    printf("鍵：0x");
+                                    for (i=0; i<16; i++){
+                                        printf("%X", last_key[i]);
+                                    }
+                                    printf("\n");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }                     
+    }           
+}
